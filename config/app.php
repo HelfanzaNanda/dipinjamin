fa4895902a5fee18bf929fa12cea8eafa78df9a7
HelfanzaNanda <elvannnd@gmail.com>
2021-06-04 15:17:43 +0700

    handle cors

--- config/app.php
+++ config/app.php
@@ -166,7 +166,7 @@ return [
          * Package Service Providers...
          */
 
-		Barryvdh\Cors\ServiceProvider::class,
+		//Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Application Service Providers...
