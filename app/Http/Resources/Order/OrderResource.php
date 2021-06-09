21ad98ef3988fcb9a3ab98d939c4b7b2993ecb77
HelfanzaNanda <elvannnd@gmail.com>
2021-06-09 09:19:28 +0700

    change resource orders

--- app/Http/Resources/Order/OrderResource.php
+++ app/Http/Resources/Order/OrderResource.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Resources\Order;
 
+use App\Http\Resources\Book\BookResource;
 use App\Http\Resources\DeliveryAddresses\DeliveryAddressesResource;
+use App\Http\Resources\User\UserResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderResource extends JsonResource
@@ -17,12 +19,9 @@ class OrderResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'book_id' => $this->book_id,
-            'book_name' => $this->book->title,
-            'borrower_id' => $this->borrower_id,
-            'borrower_name' => $this->borrower->name,
-            'owner_id' => $this->owner_id,
-            'owner_name' => $this->owner->name,
+            'book' => new BookResource($this->book),
+            'borrower' => new UserResource($this->borrower),
+            'owner' => new UserResource($this->owner),
             'duration' => $this->duration,
             'first_day_borrow' => $this->first_day_borrow,
             'last_day_borrow' => $this->last_day_borrow,
